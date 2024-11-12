581d5dbaf596119aad67674050518dd6dd2de352
pnk2u <159317511+pnk2u@users.noreply.github.com>
2024-11-12 06:03:53 +0100

    Add Pale Oak Stick
    (Texture by Hunnycakehorse)

--- src/main/java/de/pnku/mstv_base/item/MoreStickVariantItems.java
+++ src/main/java/de/pnku/mstv_base/item/MoreStickVariantItems.java
@@ -20,6 +20,7 @@ public class MoreStickVariantItems {
     public static final Item CHERRY_STICK = new MoreStickVariantItem("cherry", new Item.Properties());
     public static final Item CRIMSON_STICK = new MoreStickVariantItem("crimson", new Item.Properties().fireResistant());
     public static final Item DARK_OAK_STICK = new MoreStickVariantItem("dark_oak", new Item.Properties());
+    public static final Item PALE_OAK_STICK = new MoreStickVariantItem("pale_oak", new Item.Properties());
     public static final Item JUNGLE_STICK = new MoreStickVariantItem("jungle", new Item.Properties());
     public static final Item MANGROVE_STICK = new MoreStickVariantItem("mangrove", new Item.Properties());
     public static final Item SPRUCE_STICK = new MoreStickVariantItem("spruce", new Item.Properties());
@@ -33,7 +34,8 @@ public class MoreStickVariantItems {
         registerStickItem(CHERRY_STICK, BIRCH_STICK);
         registerStickItem(CRIMSON_STICK, CHERRY_STICK);
         registerStickItem(DARK_OAK_STICK, CRIMSON_STICK);
-        registerStickItem(JUNGLE_STICK, DARK_OAK_STICK);
+        registerStickItem(PALE_OAK_STICK, DARK_OAK_STICK);
+        registerStickItem(JUNGLE_STICK, PALE_OAK_STICK);
         registerStickItem(MANGROVE_STICK, JUNGLE_STICK);
         registerStickItem(SPRUCE_STICK, MANGROVE_STICK);
         registerStickItem(WARPED_STICK, SPRUCE_STICK);
