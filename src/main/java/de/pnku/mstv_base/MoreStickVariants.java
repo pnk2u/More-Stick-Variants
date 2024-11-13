a61597298f9b7d105697657893e3358cc24b571a
pnk2u <159317511+pnk2u@users.noreply.github.com>
2024-11-14 00:18:52 +0100

    Add Pale Oak Stick

--- src/main/java/de/pnku/mstv_base/MoreStickVariants.java
+++ src/main/java/de/pnku/mstv_base/MoreStickVariants.java
@@ -3,6 +3,9 @@ package de.pnku.mstv_base;
 import de.pnku.mstv_base.item.MoreStickVariantItems;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.resources.ResourceLocation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -17,6 +20,11 @@ public class MoreStickVariants implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		MoreStickVariantItems.registerStickItems();
+		ResourceManagerHelper.registerBuiltinResourcePack(
+				ResourceLocation.fromNamespaceAndPath(MOD_ID, "pale_oak_experimental"),
+				FabricLoader.getInstance().getModContainer(MOD_ID).orElseThrow(),
+				ResourcePackActivationType.ALWAYS_ENABLED
+		);
 	}
 
 	public static ResourceLocation asId(String path) {
