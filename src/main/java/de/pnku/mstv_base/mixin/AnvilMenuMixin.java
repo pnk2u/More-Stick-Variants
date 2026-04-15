489918ab16cae424b315d4989949dbb83d6a71da
pnku <pnku@pnku.de>
2026-04-16 01:55:18 +0200

    Update AnvilMenu Enchantability Matching (1.4.15-d:c4)
    - Adds a list of enchantable Item tags and iterates to determine matching ItemStack types
    - Renames isSameToolType to isSameEnchantabilityType and includes BOW_ENCHANTABLE, CROSSBOW_ENCHANTABLE and FISHING_ENCHANTABLE among checked tags instead of C_Tags

--- src/main/java/de/pnku/mstv_base/mixin/AnvilMenuMixin.java
+++ src/main/java/de/pnku/mstv_base/mixin/AnvilMenuMixin.java
@@ -6,9 +6,13 @@ import net.minecraft.world.inventory.AnvilMenu;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TieredItem;
+import net.minecraft.tags.TagKey;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static net.minecraft.tags.ItemTags.*;
 import static net.fabricmc.fabric.api.tag.convention.v2.ConventionalItemTags.*;
@@ -19,7 +23,7 @@ public abstract class AnvilMenuMixin {
     private boolean wrappedCreateResultItemStackIs(ItemStack firstInputStack, Item secondInputItem, Operation<Boolean> original) {
         if (original.call(firstInputStack, secondInputItem)) return true;
 
-        if (isSameToolType(firstInputStack, new ItemStack(secondInputItem))) {
+        if (isSameEnchantabilityType(firstInputStack, new ItemStack(secondInputItem))) {
             if (firstInputStack.getItem() instanceof TieredItem firstTiered && secondInputItem instanceof TieredItem secondTiered) {
                 return firstTiered.getTier() == secondTiered.getTier();
             }
@@ -29,16 +33,14 @@ public abstract class AnvilMenuMixin {
     }
     
     @Unique
-    private boolean isSameToolType(ItemStack stack1, ItemStack stack2) {
-        return (stack1.is(AXES) && stack2.is(AXES))
-                || (stack1.is(HOES) && stack2.is(HOES))
-                || (stack1.is(PICKAXES) && stack2.is(PICKAXES))
-                || (stack1.is(SHOVELS) && stack2.is(SHOVELS))
-                || (stack1.is(SWORDS) && stack2.is(SWORDS))
-                || (stack1.is(BOW_TOOLS) && stack2.is(BOW_TOOLS))
-                || (stack1.is(CROSSBOW_TOOLS) && stack2.is(CROSSBOW_TOOLS))
-                || (stack1.is(FISHING_ROD_TOOLS) && stack2.is(FISHING_ROD_TOOLS))
-                || (stack1.is(SHIELD_TOOLS) && stack2.is(SHIELD_TOOLS))
-                || (stack1.is(SPEAR_TOOLS) && stack2.is(SPEAR_TOOLS));
+    private boolean isSameEnchantabilityType(ItemStack stack1, ItemStack stack2) {
+        List <TagKey<Item>> enchantableTags = new ArrayList<>();
+        enchantableTags.add(AXES); enchantableTags.add(HOES); enchantableTags.add(PICKAXES); enchantableTags.add(SHOVELS); enchantableTags.add(SWORDS);
+        enchantableTags.add(BOW_ENCHANTABLE); enchantableTags.add(CROSSBOW_ENCHANTABLE); enchantableTags.add(FISHING_ENCHANTABLE);
+        enchantableTags.add(SHIELD_TOOLS); enchantableTags.add(SPEAR_TOOLS);
+        for (TagKey<Item> tag : enchantableTags) {
+            if (stack1.is(tag) && stack2.is(tag)) return true;
+        }
+        return false;
     }
 }
